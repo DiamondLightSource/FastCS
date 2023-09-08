d552bbbdc41b472816594e6c85b4564bf6b86536
MJGaughran <MJGaughran@users.noreply.github.com>
2023-09-08 16:17:06 +0000

    deploy: 00f04e1d27650b86016ba143ade12e21426c2e90

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1a4.dev2+g0cbc02d',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1a5.dev20+g00f04e1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
