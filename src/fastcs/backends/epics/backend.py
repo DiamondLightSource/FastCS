ffd91e120378618506791e0a8ea7901187844203
Gary Yendell <gary.yendell@diamond.ac.uk>
2024-08-07 10:37:25 +0000

    Update EPICS Backend
    Re-order methods

--- src/fastcs/backends/epics/backend.py
+++ src/fastcs/backends/epics/backend.py
@@ -1,22 +1,23 @@
-from fastcs.mapping import Mapping
+from fastcs.backend import Backend
+from fastcs.controller import Controller
 
 from .docs import EpicsDocs, EpicsDocsOptions
 from .gui import EpicsGUI, EpicsGUIOptions
-from .ioc import EpicsIOC
+from .ioc import EpicsIOC, EpicsIOCOptions
 
 
-class EpicsBackend:
-    def __init__(self, mapping: Mapping, pv_prefix: str = "MY-DEVICE-PREFIX"):
-        self._mapping = mapping
+class EpicsBackend(Backend):
+    def __init__(self, controller: Controller, pv_prefix: str = "MY-DEVICE-PREFIX"):
+        super().__init__(controller)
+
         self._pv_prefix = pv_prefix
+        self._ioc = EpicsIOC(pv_prefix, self._mapping)
 
     def create_docs(self, options: EpicsDocsOptions | None = None) -> None:
-        docs = EpicsDocs(self._mapping)
-        docs.create_docs(options)
+        EpicsDocs(self._mapping).create_docs(options)
 
     def create_gui(self, options: EpicsGUIOptions | None = None) -> None:
-        gui = EpicsGUI(self._mapping, self._pv_prefix)
-        gui.create_gui(options)
+        EpicsGUI(self._mapping, self._pv_prefix).create_gui(options)
 
-    def get_ioc(self) -> EpicsIOC:
-        return EpicsIOC(self._mapping, self._pv_prefix)
+    def _run(self, options: EpicsIOCOptions | None = None):
+        self._ioc.run(self._dispatcher, self._context, options)
--- src/fastcs/backends/epics/ioc.py
+++ src/fastcs/backends/epics/ioc.py
@@ -3,11 +3,11 @@ from dataclasses import dataclass
 from types import MethodType
 from typing import Any
 
-from softioc import asyncio_dispatcher, builder, softioc
+from softioc import builder, softioc
+from softioc.asyncio_dispatcher import AsyncioDispatcher
 from softioc.pythonSoftIoc import RecordWrapper
 
 from fastcs.attributes import AttrR, AttrRW, AttrW
-from fastcs.backend import Backend
 from fastcs.datatypes import Bool, DataType, Float, Int, String
 from fastcs.exceptions import FastCSException
 from fastcs.mapping import Mapping
@@ -16,6 +16,54 @@ from fastcs.mapping import Mapping
 @dataclass
 class EpicsIOCOptions:
     terminal: bool = True
+
+
+class EpicsIOC:
+    def __init__(self, pv_prefix: str, mapping: Mapping):
+        builder.SetDeviceName(pv_prefix)
+
+        _create_and_link_attribute_pvs(mapping)
+        _create_and_link_command_pvs(mapping)
+
+    def run(
+        self,
+        dispatcher: AsyncioDispatcher,
+        context: dict[str, Any],
+        options: EpicsIOCOptions | None = None,
+    ) -> None:
+        if options is None:
+            options = EpicsIOCOptions()
+
+        builder.LoadDatabase()
+        softioc.iocInit(dispatcher)
+
+        softioc.interactive_ioc(context)
+
+
+def _create_and_link_attribute_pvs(mapping: Mapping) -> None:
+    for single_mapping in mapping.get_controller_mappings():
+        path = single_mapping.controller.path
+        for attr_name, attribute in single_mapping.attributes.items():
+            attr_name = attr_name.title().replace("_", "")
+            pv_name = f"{':'.join(path)}:{attr_name}" if path else attr_name
+
+            match attribute:
+                case AttrRW():
+                    _create_and_link_read_pv(pv_name + "_RBV", attribute)
+                    _create_and_link_write_pv(pv_name, attribute)
+                case AttrR():
+                    _create_and_link_read_pv(pv_name, attribute)
+                case AttrW():
+                    _create_and_link_write_pv(pv_name, attribute)
+
+
+def _create_and_link_read_pv(pv_name: str, attribute: AttrR) -> None:
+    record = _get_input_record(pv_name, attribute.datatype)
+
+    async def async_wrapper(v):
+        record.set(v)
+
+    attribute.set_update_callback(async_wrapper)
 
 
 def _get_input_record(pv_name: str, datatype: DataType) -> RecordWrapper:
@@ -32,13 +80,15 @@ def _get_input_record(pv_name: str, datatype: DataType) -> RecordWrapper:
             raise FastCSException(f"Unsupported type {type(datatype)}: {datatype}")
 
 
-def _create_and_link_read_pv(pv_name: str, attribute: AttrR) -> None:
-    record = _get_input_record(pv_name, attribute.datatype)
+def _create_and_link_write_pv(pv_name: str, attribute: AttrW) -> None:
+    record = _get_output_record(
+        pv_name, attribute.datatype, on_update=attribute.process_without_display_update
+    )
 
     async def async_wrapper(v):
-        record.set(v)
+        record.set(v, process=False)
 
-    attribute.set_update_callback(async_wrapper)
+    attribute.set_write_display_callback(async_wrapper)
 
 
 def _get_output_record(pv_name: str, datatype: DataType, on_update: Callable) -> Any:
@@ -65,41 +115,6 @@ def _get_output_record(pv_name: str, datatype: DataType, on_update: Callable) ->
             raise FastCSException(f"Unsupported type {type(datatype)}: {datatype}")
 
 
-def _create_and_link_write_pv(pv_name: str, attribute: AttrW) -> None:
-    record = _get_output_record(
-        pv_name, attribute.datatype, on_update=attribute.process_without_display_update
-    )
-
-    async def async_wrapper(v):
-        record.set(v, process=False)
-
-    attribute.set_write_display_callback(async_wrapper)
-
-
-def _create_and_link_command_pv(pv_name: str, method: Callable) -> None:
-    async def wrapped_method(_: Any):
-        await method()
-
-    builder.aOut(pv_name, initial_value=0, always_update=True, on_update=wrapped_method)
-
-
-def _create_and_link_attribute_pvs(mapping: Mapping) -> None:
-    for single_mapping in mapping.get_controller_mappings():
-        path = single_mapping.controller.path
-        for attr_name, attribute in single_mapping.attributes.items():
-            attr_name = attr_name.title().replace("_", "")
-            pv_name = f"{':'.join(path)}:{attr_name}" if path else attr_name
-
-            match attribute:
-                case AttrRW():
-                    _create_and_link_read_pv(pv_name + "_RBV", attribute)
-                    _create_and_link_write_pv(pv_name, attribute)
-                case AttrR():
-                    _create_and_link_read_pv(pv_name, attribute)
-                case AttrW():
-                    _create_and_link_write_pv(pv_name, attribute)
-
-
 def _create_and_link_command_pvs(mapping: Mapping) -> None:
     for single_mapping in mapping.get_controller_mappings():
         path = single_mapping.controller.path
@@ -112,41 +127,8 @@ def _create_and_link_command_pvs(mapping: Mapping) -> None:
             )
 
 
-class EpicsIOC:
-    def __init__(self, mapping: Mapping, pv_prefix: str):
-        self._mapping = mapping
-        self._pv_prefix = pv_prefix
+def _create_and_link_command_pv(pv_name: str, method: Callable) -> None:
+    async def wrapped_method(_: Any):
+        await method()
 
-    def run(self, options: EpicsIOCOptions | None = None) -> None:
-        if options is None:
-            options = EpicsIOCOptions()
-
-        # Create an asyncio dispatcher; the event loop is now running
-        dispatcher = asyncio_dispatcher.AsyncioDispatcher()
-        backend = Backend(self._mapping, dispatcher.loop)
-
-        # Set the record prefix
-        builder.SetDeviceName(self._pv_prefix)
-
-        _create_and_link_attribute_pvs(self._mapping)
-
-        _create_and_link_command_pvs(self._mapping)
-
-        # Boilerplate to get the IOC started
-        builder.LoadDatabase()
-        softioc.iocInit(dispatcher)
-
-        backend.link_process_tasks()
-        backend.run_initial_tasks()
-        backend.start_scan_tasks()
-
-        # Run the interactive shell
-        global_variables = globals()
-        global_variables.update(
-            {
-                "dispatcher": dispatcher,
-                "mapping": self._mapping,
-                "controller": self._mapping.controller,
-            }
-        )
-        softioc.interactive_ioc(globals())
+    builder.aOut(pv_name, initial_value=0, always_update=True, on_update=wrapped_method)
