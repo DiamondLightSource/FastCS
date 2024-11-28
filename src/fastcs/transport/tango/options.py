333d15215f66f60893a8ab5f0d183e28cee6d256
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2024-11-28 11:16:00 +0000

    Remove unused tango option

--- src/fastcs/transport/tango/options.py
+++ src/fastcs/transport/tango/options.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass, field
 @dataclass
 class TangoDSROptions:
     dev_name: str = "MY/DEVICE/NAME"
-    dev_class: str = "FAST_CS_DEVICE"
     dsr_instance: str = "MY_SERVER_INSTANCE"
     debug: bool = False
 
