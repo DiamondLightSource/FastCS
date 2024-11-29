a2389376a85eb2150723b47a1813f519f52d12e7
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2024-11-29 13:21:27 +0000

    Fix import namespace

--- src/fastcs/connections/__init__.py
+++ src/fastcs/connections/__init__.py
@@ -1,7 +1,5 @@
-from .ip_connection import IPConnection
+from .ip_connection import IPConnection as IPConnection
 from .ip_connection import IPConnectionSettings as IPConnectionSettings
 from .ip_connection import StreamConnection as StreamConnection
 from .serial_connection import SerialConnection as SerialConnection
 from .serial_connection import SerialConnectionSettings as SerialConnectionSettings
-
-__all__ = ["IPConnection"]
