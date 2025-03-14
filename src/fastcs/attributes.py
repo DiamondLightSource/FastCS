3dc15a4028a043ac949baa338ae0488f3f4fc882
Alan Greer <alan.greer@diamond.ac.uk>
2025-03-14 16:33:18 +0000

    Split initialise method and store controller into base class

--- src/fastcs/attributes.py
+++ src/fastcs/attributes.py
@@ -19,10 +19,20 @@ class AttrMode(Enum):
 class Sender(Protocol):
     """Protocol for setting the value of an ``Attribute``."""
 
-    async def initialise(self, controller: Any):
+    # Record the controller that owns this attribute handler
+    controller: Any = None
+
+    async def initialise(self, controller: Any) -> None:
+        # Register the controller
+        self.controller = controller
+
+        # Continue with initialisation
+        await self._initialise()
+
+    async def _initialise(self) -> None:
         pass
 
-    async def put(self, controller: Any, attr: AttrW, value: Any) -> None:
+    async def put(self, attr: AttrW, value: Any) -> None:
         pass
 
 
@@ -30,13 +40,25 @@ class Sender(Protocol):
 class Updater(Protocol):
     """Protocol for updating the cached readback value of an ``Attribute``."""
 
+    # Record the controller that owns this attribute handler
+    controller: Any = None
+
     # If update period is None then the attribute will not be updated as a task.
     update_period: float | None = None
 
-    async def initialise(self, controller: Any):
+    async def initialise(self, controller: Any) -> None:
         pass
 
-    async def update(self, controller: Any, attr: AttrR) -> None:
+        # Register the controller
+        self.controller = controller
+
+        # Continue with initialisation
+        await self._initialise()
+
+    async def _initialise(self) -> None:
+        pass
+
+    async def update(self, attr: AttrR) -> None:
         pass
 
 
@@ -50,13 +72,13 @@ class Handler(Sender, Updater, Protocol):
 class SimpleHandler(Handler):
     """Handler for internal parameters"""
 
-    async def put(self, controller: Any, attr: AttrW, value: Any):
+    async def put(self, attr: AttrW, value: Any):
         await attr.update_display_without_process(value)
 
         if isinstance(attr, AttrRW):
             await attr.set(value)
 
-    async def update(self, controller: Any, attr: AttrR):
+    async def update(self, attr: AttrR):
         raise RuntimeError("SimpleHandler cannot update")
 
 
