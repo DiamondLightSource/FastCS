8ccc575ea3fef1488d757e800bf8111eb847cb89
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2025-03-28 13:24:50 +0000

    Revert "Add iocinit flag to disable pva serving"
    This reverts commit 862ea4cf1c1b43a2ea966168932498d38817afe2.

--- src/fastcs/transport/epics/ca/ioc.py
+++ src/fastcs/transport/epics/ca/ioc.py
@@ -47,7 +47,7 @@ class EpicsCAIOC:
     ) -> None:
         dispatcher = AsyncioDispatcher(loop)  # Needs running loop
         builder.LoadDatabase()
-        softioc.iocInit(dispatcher, enable_pva=False)
+        softioc.iocInit(dispatcher)
 
 
 def _add_pvi_info(
