861d4b92599b5804efc77345026be013a99a5509
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2025-04-04 09:03:10 +0100

    PR comments

--- tests/test_util.py
+++ tests/test_util.py
@@ -22,7 +22,7 @@ def test_snake_to_pascal():
     assert snake_to_pascal(name7) == "NameInLowerCase"
 
 
-def test_pvi__validation_error():
+def test_pvi_validation_error():
     name = snake_to_pascal("Name-With_%_Invalid-&-Symbols_£_")
     with pytest.raises(ValidationError):
         SignalR(name=name, read_pv="test")
