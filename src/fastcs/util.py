30e8ef4f97968912db381455d77f02448cc85c05
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2025-04-04 09:03:10 +0100

    PR comments

--- src/fastcs/util.py
+++ src/fastcs/util.py
@@ -2,6 +2,9 @@ import re
 
 
 def snake_to_pascal(name: str) -> str:
+    """Converts string from snake case to Pascal case.
+    If string already in Pascal case it's returned unchanged
+    """
     name = re.sub(
         r"(?:^|_)([a-z])", lambda match: match.group(1).upper(), name
     ).replace("_", "")
