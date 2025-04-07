c5143eedf87d807a6537d878cca2c1787c5585b3
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2025-04-07 15:25:28 +0100

    PR comments
    Made function only convert strings that are valid snake case and removed
    redundant code parts.

--- src/fastcs/util.py
+++ src/fastcs/util.py
@@ -5,7 +5,8 @@ def snake_to_pascal(name: str) -> str:
     """Converts string from snake case to Pascal case.
     If string already in Pascal case it's returned unchanged
     """
-    name = re.sub(
-        r"(?:^|_)([a-z])", lambda match: match.group(1).upper(), name
-    ).replace("_", "")
-    return re.sub(r"_(\d+)$", r"\1", name)
+    if re.fullmatch(r"[a-z]+(?:_[a-z]+|_[0-9]+)*", name):
+        name = re.sub(
+            r"(?:^|_)([a-z]|[0-9])", lambda match: match.group(1).upper(), name
+        )
+    return name
--- tests/test_util.py
+++ tests/test_util.py
@@ -6,20 +6,24 @@ from fastcs.util import snake_to_pascal
 
 
 def test_snake_to_pascal():
-    name1 = "a-b-c-d-e"
-    name2 = "a_b_c_d_e"
+    name1 = "name_in_snake_case"
+    name2 = "name-not-in-snake-case"
     name3 = "name_with-different_separators"
-    name4 = "1_2_3-a-b-c"
-    name5 = "NameAlreadyInPascalCase"
-    name6 = "Name-With_%_Invalid-&-Symbols_£_"
-    name7 = "name_in_lower_case"
-    assert snake_to_pascal(name1) == "A-b-c-d-e"
-    assert snake_to_pascal(name2) == "ABCDE"
-    assert snake_to_pascal(name3) == "NameWith-differentSeparators"
-    assert snake_to_pascal(name4) == "123-a-b-c"
-    assert snake_to_pascal(name5) == "NameAlreadyInPascalCase"
-    assert snake_to_pascal(name6) == "Name-With%Invalid-&-Symbols£"
-    assert snake_to_pascal(name7) == "NameInLowerCase"
+    name4 = "name_with_numbers_1_2_3"
+    name5 = "numbers_1_2_3_in_the_middle"
+    name6 = "1_2_3_starting_with_numbers"
+    name7 = "Name_With_%_Invalid_&_Symbols_£_"
+    name8 = "name_in_lower_case"
+    name9 = "NameAlreadyInPascalCase"
+    assert snake_to_pascal(name1) == "NameInSnakeCase"
+    assert snake_to_pascal(name2) == "name-not-in-snake-case"
+    assert snake_to_pascal(name3) == "name_with-different_separators"
+    assert snake_to_pascal(name4) == "NameWithNumbers123"
+    assert snake_to_pascal(name5) == "Numbers123InTheMiddle"
+    assert snake_to_pascal(name6) == "1_2_3_starting_with_numbers"
+    assert snake_to_pascal(name7) == "Name_With_%_Invalid_&_Symbols_£_"
+    assert snake_to_pascal(name8) == "NameInLowerCase"
+    assert snake_to_pascal(name9) == "NameAlreadyInPascalCase"
 
 
 def test_pvi_validation_error():
