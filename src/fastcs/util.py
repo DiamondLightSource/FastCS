c75aec6e2c7a8cd5ace4b827ff6575ef892f98a9
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2025-04-08 09:16:57 +0100

    Updated function description

--- src/fastcs/util.py
+++ src/fastcs/util.py
@@ -3,7 +3,7 @@ import re
 
 def snake_to_pascal(name: str) -> str:
     """Converts string from snake case to Pascal case.
-    If string already in Pascal case it's returned unchanged
+    If string is not a valid snake case it will be returned unchanged
     """
     if re.fullmatch(r"[a-z]+(?:_[a-z]+|_[0-9]+)*", name):
         name = re.sub(
