5e5932208e21d24ebffd969557fdd2d7810a4316
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2025-05-23 10:52:45 +0100

    Updated regex and tests

--- src/fastcs/util.py
+++ src/fastcs/util.py
@@ -5,6 +5,6 @@ def snake_to_pascal(name: str) -> str:
     """Converts string from snake case to Pascal case.
     If string is not a valid snake case it will be returned unchanged
     """
-    if re.fullmatch(r"[a-z]+(?:_[a-z0-9]+)*", name):
+    if re.fullmatch(r"[a-z][a-z0-9]*(?:_[a-z0-9]+)*", name):
         name = re.sub(r"(?:^|_)([a-z0-9])", lambda match: match.group(1).upper(), name)
     return name
