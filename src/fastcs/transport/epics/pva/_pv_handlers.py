89ef55e1213f18c717c8c3350fe360c6e21bc779
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2025-07-23 15:27:07 +0000

    Remove old make shared pv function

--- src/fastcs/transport/epics/pva/_pv_handlers.py
+++ src/fastcs/transport/epics/pva/_pv_handlers.py
@@ -13,7 +13,6 @@ from fastcs.datatypes import Table
 from .types import (
     MAJOR_ALARM_SEVERITY,
     RECORD_ALARM_STATUS,
-    T,
     cast_from_p4p_value,
     cast_to_p4p_value,
     make_p4p_type,
@@ -135,39 +134,6 @@ def make_shared_write_pv(attribute: AttrW) -> SharedPV:
         shared_pv.post(cast_to_p4p_value(attribute, value))
 
     attribute.add_write_display_callback(async_write_display)
-
-    return shared_pv
-
-
-def make_shared_pv(attribute: Attribute) -> SharedPV:
-    initial_value = (
-        attribute.get()
-        if isinstance(attribute, AttrR)
-        else attribute.datatype.initial_value
-    )
-
-    type_ = make_p4p_type(attribute)
-    kwargs = {"initial": cast_to_p4p_value(attribute, initial_value)}
-    if isinstance(type_, (NTEnum | NTNDArray | NTTable)):
-        kwargs["nt"] = type_
-    else:
-
-        def _wrap(value: dict):
-            return Value(type_, value)
-
-        kwargs["wrap"] = _wrap
-
-    if isinstance(attribute, AttrW):
-        kwargs["handler"] = WritePvHandler(attribute)
-
-    shared_pv = SharedPV(**kwargs)
-
-    if isinstance(attribute, AttrR):
-
-        async def on_update(value):
-            shared_pv.post(cast_to_p4p_value(attribute, value))
-
-        attribute.add_update_callback(on_update)
 
     return shared_pv
 
