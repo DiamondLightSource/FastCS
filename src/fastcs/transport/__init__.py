b7ac9115ba266d7ece7389544a27b3968fcc7a0d
Gary Yendell <gary.yendell@diamond.ac.uk>
2025-10-03 18:06:25 +0100

    Fix Transport imports (#227)
    Make import of all transports conditional
    Build Union of Transports that get imported
    Error if no imports are available for now

--- src/fastcs/transport/__init__.py
+++ src/fastcs/transport/__init__.py
@@ -1,10 +1,30 @@
-from .epics.ca.transport import EpicsCATransport as EpicsCATransport
-from .epics.options import EpicsDocsOptions as EpicsDocsOptions
-from .epics.options import EpicsGUIOptions as EpicsGUIOptions
-from .epics.options import EpicsIOCOptions as EpicsIOCOptions
-from .epics.pva.transport import EpicsPVATransport as EpicsPVATransport
-from .graphql.transport import GraphQLTransport as GraphQLTransport
-from .rest.transport import RestTransport as RestTransport
-from .tango.options import TangoDSROptions as TangoDSROptions
-from .tango.transport import TangoTransport as TangoTransport
 from .transport import Transport as Transport
+
+try:
+    from .epics.ca.transport import EpicsCATransport as EpicsCATransport
+    from .epics.options import EpicsDocsOptions as EpicsDocsOptions
+    from .epics.options import EpicsGUIOptions as EpicsGUIOptions
+    from .epics.options import EpicsIOCOptions as EpicsIOCOptions
+except ImportError:
+    pass
+
+try:
+    from .epics.pva.transport import EpicsPVATransport as EpicsPVATransport
+except ImportError:
+    pass
+
+try:
+    from .graphql.transport import GraphQLTransport as GraphQLTransport
+except ImportError:
+    pass
+
+try:
+    from .rest.transport import RestTransport as RestTransport
+except ImportError:
+    pass
+
+try:
+    from .tango.options import TangoDSROptions as TangoDSROptions
+    from .tango.transport import TangoTransport as TangoTransport
+except ImportError:
+    pass
