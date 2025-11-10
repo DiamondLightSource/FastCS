d7b2684879ad3ff4735295102f3fe3b505d4c7e9
Gary Yendell <gary.yendell@diamond.ac.uk>
2025-11-10 13:49:41 +0000

    Fix EpicsCATransport to pass length with initial_value for string records
    softioc sets the length of the initial value as the max length, so it
    can never be set if it is empty initially. Override with the driver set
    length or 256 by default.

--- src/fastcs/datatypes.py
+++ src/fastcs/datatypes.py
@@ -128,6 +128,9 @@ class Bool(DataType[bool]):
 class String(DataType[str]):
     """`DataType` mapping to builtin ``str``."""
 
+    length: int | None = None
+    """Maximum length of string to display in transports"""
+
     @property
     def dtype(self) -> type[str]:
         return str
