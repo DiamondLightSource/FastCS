92fb66f771f87fc477c6cfa3e7063e98bb8ff83d
Gary Yendell <gary.yendell@diamond.ac.uk>
2025-11-11 16:29:42 +0000

    Remove EpicsCAOptions class
    This is unused since #227

--- docs/snippets/static05.py
+++ docs/snippets/static05.py
@@ -4,9 +4,8 @@ from fastcs.attributes import AttrR
 from fastcs.controller import Controller
 from fastcs.datatypes import String
 from fastcs.launch import FastCS
-from fastcs.transport.epics.ca.options import EpicsGUIOptions
 from fastcs.transport.epics.ca.transport import EpicsCATransport
-from fastcs.transport.epics.options import EpicsIOCOptions
+from fastcs.transport.epics.options import EpicsGUIOptions, EpicsIOCOptions
 
 
 class TemperatureController(Controller):
