3eb02cc616aa76110f046b7f959b60164e4b6849
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-02-13 15:43:27 +0000

    Pass attribute instead of datatype into _get_read_widget

--- src/fastcs/transports/epics/gui.py
+++ src/fastcs/transports/epics/gui.py
@@ -57,8 +57,8 @@ class EpicsGUI:
         )
         return f"{attr_prefix}:{snake_to_pascal(name)}"
 
-    def _get_read_widget(self, fastcs_datatype: DataType) -> ReadWidgetUnion | None:
-        match fastcs_datatype:
+    def _get_read_widget(self, attribute: Attribute) -> ReadWidgetUnion | None:
+        match attribute.datatype:
             case Bool():
                 return LED()
             case Int():
@@ -71,7 +71,10 @@ class EpicsGUI:
                 return TextRead(format=TextFormat.string)
             case Waveform() as waveform:
                 if len(waveform.shape) > 1:
-                    logger.warning("EPICS CA transport only supports 1D waveforms")
+                    logger.warning(
+                        "EPICS CA transport only supports 1D waveforms,"
+                        + f"{attribute} is a {waveform.shape} waveform"
+                    )
                     return None
 
                 return ArrayTrace(axis="x")
@@ -102,7 +105,7 @@ class EpicsGUI:
         name = snake_to_pascal(name)
         match attribute:
             case AttrRW():
-                read_widget = self._get_read_widget(attribute.datatype)
+                read_widget = self._get_read_widget(attribute)
                 write_widget = self._get_write_widget(attribute.datatype)
                 if write_widget is None or read_widget is None:
                     return None
@@ -115,7 +118,7 @@ class EpicsGUI:
                     read_widget=read_widget,
                 )
             case AttrR():
-                read_widget = self._get_read_widget(attribute.datatype)
+                read_widget = self._get_read_widget(attribute)
                 if read_widget is None:
                     return None
                 return SignalR(
